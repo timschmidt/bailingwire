76210a558e56e2de128b278a2684eb6b353e16b7
Timothy Schmidt <timschmidt@gmail.com>
2014-09-09 22:49:30 -0400

    firm up the idle loop

---
+++ core-idleloop.h
@@ -0,0 +1,11 @@
+#ifndef CORE_IDLELOOP_H
+#define CORE_IDLELOOP_H
+
+while (nothing bad happens)
+{
+	
+	iterate through an array of function pointers to services, which have been pushed onto the array by a serial command
+	
+}
+
+#endif // CORE_IDLELOOP_H
