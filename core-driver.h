2c3971dfe18be255d7ea7c94ce8d7f0f6501a6bb
Timothy Schmidt <timschmidt@gmail.com>
2014-09-18 19:36:19 -0400

    separate device interfaces from driver implementations

--- core-driver.h
+++ core-driver.h
@@ -6,40 +6,53 @@
 // define public methods, for these objects:
 // calloc memory sufficient for a copy of the driver struct, populate it with all the state for that instance, function pointers to the public methods for that device as implemented by it's driver, and return a pointer to the memory address of the struct
 
+// Device types (function call interfaces)
+// 1: accelerometer 
+// 2: barometer
+// 3: battery - check status, time left
+// 4: color sensor - get color
+// 5: conductivity sensor - get conductivity
+// 6: current sensor - get current
+// 7: distance sensor - get distance
+// 8: extruder - extrude X mm? ml?
+// 9: fan - set fan speed (percent)
+// 10: flow sensor - get flow (volume / time)
+// 11: gpio - 
+// 12: heater - 
+// 13: humidity - 
+// 14: laser - 
+// 15: lcd - 
+// 16: led - set light intensity
+// 17: light sensor - 
+// 18: motor - move (direction, distance, time)
+// 19: ph sensor - 
+// 20: pump - 
+// 21: relay - 
+// 22: rtc - 
+// 23: sd - 
+// 24: servo - 
+// 25: soil moisture - 
+// 26: solenoid - 
+// 27: stepper - 
+// 28: switch - 
+// 29: temp sensor - 
+// 30: valve - 
+// 31: voltage sensor - 
+// 32: wind sensor - 
+
+// Drivers:
 // PROGMEM array pointing to driver initializtion functions
 array prog_char[32] PROGMEM = [
-	&init_accelerometer			// 1: accelerometer 
-	&init_barometer				// 2: barometer
-	&init_battery				// 3: battery - check status, time left
-	&init_colorsensor			// 4: color sensor - get color
-	&init_conductivitysensor	// 5: conductivity sensor - get conductivity
-	&init_currentsensor			// 6: current sensor - get current
-	&init_distancesensor		// 7: distance sensor - get distance
-	&init_extruder				// 8: extruder - extrude X mm? ml?
-	&init_fan					// 9: fan - set fan speed (percent)
-	&init_flowsensor			// 10: flow sensor - get flow (volume / time)
-	&init_gpio					// 11: gpio - 
-	&init_heater				// 12: heater - 
-	&init_humidity				// 13: humidity - 
-	&init_laser					// 14: laser - 
-	&init_lcd					// 15: lcd - 
-	&init_led					// 16: led - set light intensity
-	&init_lightsensor			// 17: light sensor - 
-	&init_motor					// 18: motor - move (direction, distance, time)
-	&init_phsensor				// 19: ph sensor - 
-	&init_pump					// 20: pump - 
-	&init_relay					// 21: relay - 
-	&init_rtc					// 22: rtc - 
-	&init_sd					// 23: sd - 
-	&init_servo					// 24: servo - 
-	&init_soilmoisture			// 25: soil moisture - 
-	&init_solenoid				// 26: solenoid - 
-	&init_stepper				// 27: stepper - 
-	&init_swtich				// 28: switch - 
-	&init_tempsensor			// 29: temp sensor - 
-	&init_valve					// 30: valve - 
-	&init_voltagesensor			// 31: voltage sensor - 
-	&init_windsensor			// 32: wind sensor - 
+	&init_8574_i2c_gpio,
+	&init_attiny,
+	&init_avreeprom,
+	&init_avrgpio,
+	&init_avrserial,
+	&init_axis,
+	&init_ftdi,
+	&init_nrf24l01,
+	&init_qtouch,
+	&init_uln2003
 ];
 
 function initialize_driver( int driver )
---
+++ driver-axis.h
@@ -0,0 +1,7 @@
+#ifndef DRIVER_AXIS_H
+#define DRIVER_AXIS_H
+
+// consumes: pointer to: one actuator, one sensor, one feedback device, and one control loop
+// returns: pointer to: one axis
+
+#endif // DRIVER_AXIS_H
