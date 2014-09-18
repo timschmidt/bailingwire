b49855dc120aa11d2d55b9cce3fa9473f1c00cfb
Timothy Schmidt <timschmidt@gmail.com>
2014-09-18 19:54:16 -0400

    fix up driver array

--- core-device-list.h
+++ core-device-list.h
@@ -4,6 +4,58 @@
 int device_count;
 int *device_list;
 
+// define public methods, for these objects:
+// calloc memory sufficient for a copy of the driver struct, populate it with all the state for that instance, function pointers to the public methods for that device as implemented by it's driver, and return a pointer to the memory address of the struct
+
+// Device types (function call interfaces)
+// 1: accelerometer 
+// 2: barometer
+// 3: battery - check status, time left
+// 4: color sensor - get color
+// 5: conductivity sensor - get conductivity
+// 6: current sensor - get current
+// 7: distance sensor - get distance
+// 8: extruder - extrude X mm? ml?
+// 9: fan - set fan speed (percent)
+// 10: flow sensor - get flow (volume / time)
+// 11: gpio - 
+// 12: heater - 
+// 13: humidity - 
+// 14: laser - 
+// 15: lcd - 
+// 16: led - set light intensity
+// 17: light sensor - 
+// 18: motor - move (direction, distance, time)
+// 19: ph sensor - 
+// 20: pump - 
+// 21: relay - 
+// 22: rtc - 
+// 23: sd - 
+// 24: servo - 
+// 25: soil moisture - 
+// 26: solenoid - 
+// 27: stepper - 
+// 28: switch - 
+// 29: temp sensor - 
+// 30: valve - 
+// 31: voltage sensor - 
+// 32: wind sensor - 
+
+// Drivers:
+// Array pointing to driver initializtion functions
+void (*drivers[10]) = {
+	&init_8574_i2c_gpio,
+	&init_attiny,
+	&init_avreeprom,
+	&init_avrgpio,
+	&init_avrserial,
+	&init_axis,
+	&init_ftdi,
+	&init_nrf24l01,
+	&init_qtouch,
+	&init_uln2003
+};
+
 function init_device_list()
 {
     device_list = calloc( 1 );
@@ -15,7 +67,7 @@ function add_device( int driver )
 	// realloc's *device_list to (devices * 1 byte), updating device_list, and stores the new device's data in the last position.
 	int new_device_list = realloc(device_list, device_count + 1);
 	if (new_device_list != 0) device_list = new_device_list;
-	*(device_list + device_count) = initialize_driver(driver);
+	*(device_list + device_count) = (*drivers[driver])();
 	device_count++;
 };
 
--- core-driver.h
+++ core-driver.h
@@ -1,63 +1,6 @@
 #ifndef CORE_DEVICES_H
 #define CORE_DEVICES_H
 
-#include <avr/pgmspace.h>
 
-// define public methods, for these objects:
-// calloc memory sufficient for a copy of the driver struct, populate it with all the state for that instance, function pointers to the public methods for that device as implemented by it's driver, and return a pointer to the memory address of the struct
-
-// Device types (function call interfaces)
-// 1: accelerometer 
-// 2: barometer
-// 3: battery - check status, time left
-// 4: color sensor - get color
-// 5: conductivity sensor - get conductivity
-// 6: current sensor - get current
-// 7: distance sensor - get distance
-// 8: extruder - extrude X mm? ml?
-// 9: fan - set fan speed (percent)
-// 10: flow sensor - get flow (volume / time)
-// 11: gpio - 
-// 12: heater - 
-// 13: humidity - 
-// 14: laser - 
-// 15: lcd - 
-// 16: led - set light intensity
-// 17: light sensor - 
-// 18: motor - move (direction, distance, time)
-// 19: ph sensor - 
-// 20: pump - 
-// 21: relay - 
-// 22: rtc - 
-// 23: sd - 
-// 24: servo - 
-// 25: soil moisture - 
-// 26: solenoid - 
-// 27: stepper - 
-// 28: switch - 
-// 29: temp sensor - 
-// 30: valve - 
-// 31: voltage sensor - 
-// 32: wind sensor - 
-
-// Drivers:
-// PROGMEM array pointing to driver initializtion functions
-array prog_char[32] PROGMEM = [
-	&init_8574_i2c_gpio,
-	&init_attiny,
-	&init_avreeprom,
-	&init_avrgpio,
-	&init_avrserial,
-	&init_axis,
-	&init_ftdi,
-	&init_nrf24l01,
-	&init_qtouch,
-	&init_uln2003
-];
-
-function initialize_driver( int driver )
-{
-	
-};
 
 #endif // CORE_DEVICES_H
