4bad43482de0fe1f3eb135c6c864f4ca9fdfcdd9
Timothy Schmidt <timschmidt@gmail.com>
2014-09-19 23:03:55 -0400

    improve gpio function call api

--- driver-8574-i2c-gpio.h
+++ driver-8574-i2c-gpio.h
@@ -1,6 +1,26 @@
 #ifndef DRIVER_8754_I2C_GPIO_H
 #define DRIVER_8754_I2C_GPIO_H
 
+function set_pin(*device_descriptor, state){
+	// craft & issue i2c packet
+	int pin = *(device_descriptor + 1);
+}
 
+struct device_descriptor
+{
+	int &set_pin(int *device_descriptor, state);
+	int pin;
+	int state;
+}
+
+function init_8754_i2c_gpio(int pin)
+{
+	int i2c_gpio = calloc( sizeof(struct device_descriptor) );
+	if (i2c_gpio !=0)
+	{
+		*(i2c_gpio + 1) = pin;
+	}
+	return i2c_gpio;
+}
 
 #endif // DRIVER_8754_I2C_GPIO_H
--- driver-avr-gpio.h
+++ driver-avr-gpio.h
@@ -1,13 +1,13 @@
 #ifndef DRIVER_AVR_GPIO_H
 #define DRIVER_AVR_GPIO_H
 
-function set_pin(int pin, int state){
-	digitalWrite(pin, state);
+function set_pin(int *device_descriptor, int state){
+	digitalWrite(*(device_descriptor + 1), state);
 }
 
 struct device_descriptor
 {
-	int &set_pin(int pin, int state);
+	int &set_pin(int *device_descriptor, int state);
 	int pin;
 	int state;
 }
