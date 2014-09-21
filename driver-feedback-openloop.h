31812c828baf76c40ecc2a46c4e8bf30088cb893
Timothy Schmidt <timschmidt@gmail.com>
2014-09-21 15:01:36 -0400

    stub out feedback handlers

--- driver-feedback-openloop.h
+++ driver-feedback-openloop.h
@@ -1,5 +1,4 @@
-/*
- *  This file is part of the bailingwire firmware.
+/*  This file is part of the bailingwire firmware.
  *
  *  Bailingwire is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -18,6 +17,33 @@
 #ifndef DRIVER_FEEDBACK_OPENLOOP_H
 #define DRIVER_FEEDBACK_OPENLOOP_H
 
+/* Open Loop feedback is a stub which does nothing.
+ */
 
+struct device_descriptor
+{
+	// Public API
+	int device_type = DEVICE_TYPE_FEEDBACK;
+	int &update(int *device_desriptor, int state);
+	int pin;
+	int state;
+	
+	// Private API
+}
+
+function alloc_feedback_openloop()
+{
+	return calloc( sizeof(struct device_descriptor) );
+}
+
+function init_feedback_openloop(int *device_descriptor)
+{
+
+}
+
+function update(int *device_descriptor, int state)
+{
+
+}
 
 #endif // DRIVER_FEEDBACK_OPENLOOP_H
--- driver-feedback-quadrature.h
+++ driver-feedback-quadrature.h
@@ -1,5 +1,4 @@
-/*
- *  This file is part of the bailingwire firmware.
+/*  This file is part of the bailingwire firmware.
  *
  *  Bailingwire is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -18,6 +17,35 @@
 #ifndef DRIVER_FEEDBACK_QUADRATURE_H
 #define DRIVER_FEEDBACK_QUADRATURE_H
 
+/* Quadrature feedback contains enough information to determine speed and direction
+ */
 
+struct device_descriptor
+{
+	// Public API
+	int device_type = DEVICE_TYPE_FEEDBACK;
+	int &update(int *device_desriptor, int state);
+	int pin;
+	int state;
+	
+	// Private API
+	float speed;
+	float direction;
+}
+
+function alloc_feedback_quadrature()
+{
+	return calloc( sizeof(struct device_descriptor) );
+}
+
+function init_feedback_quadrature(int *device_descriptor, int pin, int state)
+{
+	
+}
+
+function update(int *device_descriptor, int state)
+{
+	
+}
 
 #endif // DRIVER_FEEDBACK_QUADRATURE_H
